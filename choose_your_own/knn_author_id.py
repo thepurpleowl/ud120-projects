feae6fd9a183c25a61c00d9a4e2127e7d63727af
thepurpleowl <suryathe16@gmail.com>
2016-12-26 13:35:16 +0530

    ADD knn,r_f,ad_b

---
+++ choose_your_own/knn_author_id.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Dec 26 12:45:24 2016
+
+@author: surya
+"""
+
+import sys
+from time import time
+sys.path.append("../tools/")
+from email_preprocess import preprocess
+features_train, features_test, labels_train, labels_test = preprocess()
+
+from sklearn.neighbors import KNeighborsClassifier
+clf=KNeighborsClassifier(n_neighbors=5, weights='distance')
+
+t0= time()
+clf.fit(features_train,labels_train)
+print "training time:", round(time()-t0, 3), "s"
+
+t1= time()
+pred=clf.predict(features_test)
+print "prediction time:", round(time()-t1, 3), "s"
+
+from sklearn.metrics import accuracy_score
+accuracy=accuracy_score(labels_test,pred)
+print accuracy
